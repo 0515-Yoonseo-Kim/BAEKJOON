7933b433a98747466801999a369379946076f811
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-06-01 03:22:11 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,17 +1,3 @@
 def solution(tickets):
-    from collections import defaultdict
-    
-    graph = defaultdict(list)
-    for start, end in sorted(tickets):
-        graph[start].append(end)
-    
-    route = []
-    def dfs(airport):
-        while graph[airport]:
-            dfs(graph[airport].pop(0))
-        route.append(airport)
-    
-    dfs("ICN")
-    return route[::-1]
-
-
+    answer = []
+    return answer
