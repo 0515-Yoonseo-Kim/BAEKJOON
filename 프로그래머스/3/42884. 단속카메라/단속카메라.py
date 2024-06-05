49f0b28dd38cf0099b12200be291e7a69ce63e79
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-06-06 00:58:54 +0900

    [level 3] Title: 단속카메라, Time: 2.25 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,11 +1,12 @@
-def solution(routes):
+def solution(routes:list):
+    ans=1
     routes.sort()
-    cam = routes[0][1]
-    answer = 1
-    for i in range(1, len(routes)):
-        if routes[i][0] > cam:
-            answer += 1
-            cam = routes[i][1]
+    camera=routes[0][1]
+    for i in range(1,len(routes)):
+        st,ed=routes[i]
+        if st>camera:
+            ans+=1
+            camera=ed
         else:
-            cam = min(cam, routes[i][1])
-    return answer
+            camera=min(camera,ed)
+    return ans
