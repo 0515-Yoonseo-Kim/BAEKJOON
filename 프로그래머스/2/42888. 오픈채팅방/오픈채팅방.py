bef509747fe24d0cd6756b2c61eee7032a7ee358
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-06-16 18:46:23 +0900

    [level 2] Title: 오픈채팅방, Time: 77.09 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,31 +1,27 @@
 from collections import defaultdict
-userdb = defaultdict(list)
+userdb = dict()
 message = []
 
 def operator(command: str):
     command = command.split()
     if command[0] == "Enter":
-        userdb[command[1]].extend([command[-1]])
+        userdb[command[1]]=command[-1]
         message.append([1,command[1]])
     elif command[0] == "Leave":
         message.append([0,command[-1]])
         
     elif command[0]  == "Change":
-        userdb[command[1]].extend([command[-1]])
+        userdb[command[1]]=command[-1]
     return 
 
 def str_mapping(li):
     c,i = li
     if c==1:
-        return f"{userdb[i][-1]}님이 들어왔습니다."
+        return f"{userdb[i]}님이 들어왔습니다."
     else:
-        return f"{userdb[i][-1]}님이 나갔습니다."
+        return f"{userdb[i]}님이 나갔습니다."
 
 def solution(record):
-    answer = []
     for rec in record:
         operator(rec)
-    for m in message:
-        a = str_mapping(m)
-        answer.append(a)
-    return answer
+    return list(map(str_mapping, message))
