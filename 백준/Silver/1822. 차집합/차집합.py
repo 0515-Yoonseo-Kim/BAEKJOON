a617d0fac3b78d9a58e9a0ddcfc3f8521429a679
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-06-20 10:23:04 +0900

    [Silver IV] Title: 차집합, Time: 952 ms, Memory: 146304 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -1,8 +1,8 @@
 A,B = map(int,input().split())
 
-set_A = set(input().split())
-set_B = set(input().split())
+set_A = set(map(int,input().split()))
+set_B = set(map(int,input().split()))
 
 diff = set_A - set_B
 print(len(diff))
-print(*sorted([int(i) for i in diff]))
+print(*sorted(diff))
