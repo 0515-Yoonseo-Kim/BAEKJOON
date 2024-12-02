70caf01fb062c49f120eddf685156b8b7da35c71
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-12-02 12:12:12 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,7 +1,12 @@
-def solution(dots):
+'''def solution(dots):
     x_set, y_set = set(), set()
     for dot in dots:
         x,y = dot
         x_set.add(x)
         y_set.add(y)
-    return abs(x_set.pop()-x_set.pop())*abs(y_set.pop()-y_set.pop())
+    return abs(x_set.pop()-x_set.pop())*abs(y_set.pop()-y_set.pop())'''
+
+def solution(dots):
+    x_diff = max(dots)[0]-min(dots)[0]
+    y_diff = max(dots)[1]-min(dots)[1]
+    return x_diff*y_diff
