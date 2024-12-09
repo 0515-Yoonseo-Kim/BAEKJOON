e0c62e4b1ae33f211c670f768da0a77d85cd94af
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-12-10 01:56:40 +0900

    [Gold IV] Title: 알파벳, Time: 6232 ms, Memory: 162352 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -6,17 +6,17 @@ arr = [list(input()) for _ in range(R)]
 directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 max_val = 0
 
-def dfs(visited: set, r: int, c: int) -> None:
+def dfs(visited: str, r: int, c: int) -> None:
     global max_val
     max_val = max(max_val, len(visited))
 
     for dr, dc in directions:
         nr, nc = r + dr, c + dc
         if 0 <= nr < R and 0 <= nc < C and arr[nr][nc] not in visited:
-            visited.add(arr[nr][nc])
+            visited+= arr[nr][nc]
             dfs(visited, nr, nc)
-            visited.remove(arr[nr][nc])
+            visited = visited[:-1]
 
-dfs(set(arr[0][0]), 0, 0)
+dfs(arr[0][0], 0, 0)
 
 print(max_val)
