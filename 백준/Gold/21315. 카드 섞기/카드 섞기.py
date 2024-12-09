b847fcea0502df3b0da4e4db6f240644cdb1f20c
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2024-12-09 19:42:56 +0900

    [Gold V] Title: 카드 섞기, Time: 100 ms, Memory: 40496 KB -BaekjoonHub

--- 백준/Gold/21315. 카드 섞기/카드 섞기.py
+++ 백준/Gold/21315. 카드 섞기/카드 섞기.py
@@ -1,29 +1,29 @@
-import math,sys
+import math
 from typing import List
 
 N = int(input())
 result = list(map(int,input().split()))
-max_k = int(math.log2(N))
+max_k = int(math.log2(N)) # max_k : 2**k<=N을 만족하는 k의 최댓값 
 
-def mix_card(li:List[int],k:int,goal:int):
+# 카드 복원 함수
+def restore_card(li:List[int],k:int,goal:int)-> List[int]:
     if k > goal:
         li = li[2**goal:]+li[:2**goal]
         return li
-    dummy = li[:2**k]
-    remains = li[2**k:]
+    dummy = li[:2**k] # 섞는 카드 더미
+    remains = li[2**k:] # 안섞는 카드 더미
     new_dummy = dummy[len(dummy)//2:] + dummy[:len(dummy)//2]
-    return mix_card(new_dummy+remains,k+1,goal)
+    return restore_card(new_dummy+remains,k+1,goal) # 재귀
 
-ans_list = []
-check = False
+
+check = False # 이중 루프 탈출용
+original = [i for i in range(1,N+1)]
 for k1 in range(1,max_k+1):
     for k2 in range(1,max_k+1):
-        ans = mix_card(mix_card(result,1,k2),1,k1)
-        if ans == [i for i in range(1,N+1)]:
-            ans_list+=[k1,k2]
+        ans = restore_card(restore_card(result,1,k2),1,k1) # 두 번 섞어서
+        if ans == original:
+            print(k1,k2)
             check=True
-            break
+            break ## 정해진 조합이 나오면 탈출
     if check:
         break
-
-print(*ans_list)
