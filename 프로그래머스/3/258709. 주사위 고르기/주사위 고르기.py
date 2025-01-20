db11df62797f47790e083ad99cdf3fcef8de0e4f
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-01-20 21:08:58 +0900

    [level 3] Title: 주사위 고르기, Time: 1590.36 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,23 @@
+from itertools import combinations, product
+from bisect import bisect_left
+
+def dice_sum_combination(dice_list):
+    return [sum(p) for p in product(*dice_list)]
+    
+def solution(dice):
+    N = len(dice)
+    dice_combination_lists = list(combinations(list(range(N)),N//2))
+    
+    max_winnings = 0
+    max_combinations = []
+    
+    for combination in dice_combination_lists:
+        a_dice_sum = dice_sum_combination(list(map(lambda i: dice[i], combination)))
+        b_dice_sum = sorted(dice_sum_combination(list(map(lambda i: dice[i], filter(lambda x: x not in combination, range(N))))))
+        
+        wins = sum(bisect_left(b_dice_sum, a_sum) for a_sum in a_dice_sum)
+        if wins > max_winnings:
+            max_winnings = wins
+            max_combinations = combination
+    
+    return [idx + 1 for idx in max_combinations]
