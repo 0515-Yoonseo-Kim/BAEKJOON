049de87fc29b3cc970fc85886c3cd59b0ad9c004
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-02-04 10:03:35 +0900

    [Gold IV] Title: N-Queen, Time: 10812 ms, Memory: 121720 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -4,11 +4,23 @@ columns = {num for num in range(N)}
 def bt(row,cols,diag1,diag2):
     if row == N:
         return 1
+    
     cnt = 0
     for col in range(N):
         if (col in cols) or ((row-col) in diag1) or ((row+col) in diag2):
             continue
-        cnt += bt(row+1,cols|{col},diag1|{row-col},diag2|{row+col})
+
+        cols.add(col)
+        diag1.add(row-col)
+        diag2.add(row+col)
+
+        cnt += bt(row+1,cols,diag1,diag2)
+
+        cols.remove(col)
+        diag1.remove(row-col)
+        diag2.remove(row+col)
+        
+        
     return cnt
 
 print(bt(0,set(),set(),set()))
