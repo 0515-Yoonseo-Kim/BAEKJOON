c9a26b50a9804e8c7a5c0b902d7c5ad6695983e4
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-02-05 13:43:48 +0900

    [level 3] Title: 스타 수열, Time: 829.57 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.py
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.py
@@ -1,24 +1,20 @@
 from collections import Counter
 
 def solution(a):
-    if not a:  # 빈 리스트일 경우 0 반환
-        return 0
+    target_dict = Counter(a)
+    max_length = 0
 
-    counter = Counter(a)  # 각 숫자의 등장 횟수 계산
-    max_length = 0  # 스타 수열의 최대 길이
-
-    for target in counter:  # 모든 숫자를 target으로 시도
-        if counter[target] * 2 <= max_length:  # 최대 길이를 넘을 가능성이 없으면 건너뛰기
+    for target in target_dict:
+        if target_dict[target] * 2 <= max_length:
             continue
-        
-        length = 0  # 현재 target으로 구성할 수 있는 스타 수열의 길이
+        length = 0
         i = 0
         while i < len(a) - 1:
-            if (a[i] == target or a[i+1] == target) and a[i] != a[i+1]:  # 조건을 만족하는 경우
+            if (a[i] == target or a[i+1] == target) and a[i] != a[i+1]:
                 length += 2
-                i += 1  # 다음 숫자로 넘어감
+                i += 1
             i += 1
 
-        max_length = max(max_length, length)  # 최댓값 갱신
+        max_length = max(max_length, length)
 
     return max_length
