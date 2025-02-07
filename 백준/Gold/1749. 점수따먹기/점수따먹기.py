80e90e6d9da5ba7debca9d0f774e4368aeafb0fc
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-02-07 14:46:45 +0900

    [Gold IV] Title: 점수따먹기, Time: 152 ms, Memory: 111100 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.py
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.py
@@ -5,19 +5,14 @@ N, M = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-max_value = -float('inf')  # 최댓값 초기화
+max_value = -float('inf')
 
-# top부터 bottom까지의 행들을 고정하여 처리
 for top in range(N):  
-    temp = [0] * M  # 각 열에 대한 부분 합을 저장할 배열
+    temp = [0] * M
     
-    # bottom 행을 고정하여, top 행부터 bottom 행까지의 합을 구함
     for bottom in range(top, N):
-        # 각 열에 대해 해당하는 부분 합을 계산
         for col in range(M):
             temp[col] += arr[bottom][col]
-        
-        # temp 배열에 대해 Kadane's Algorithm을 적용하여 최대 부분합을 구함
         current_sum = 0
         local_max = -float('inf')
         
@@ -28,8 +23,6 @@ for top in range(N):
             if current_sum < 0:
                 current_sum = 0
         
-        # 최댓값 갱신
         max_value = max(max_value, local_max)
 
-# 출력
 print(max_value)
