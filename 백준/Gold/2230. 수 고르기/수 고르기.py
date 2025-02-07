0c616dada667b0b0227d5c55b8938e96b3a4418f
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-02-07 21:13:32 +0900

    [Gold V] Title: 수 고르기, Time: 152 ms, Memory: 39116 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,18 +1,14 @@
 import sys
-import heapq
+import bisect
 input = sys.stdin.readline
+
+min_value = int(2*1e9)
 
 N, M = map(int,input().split())
 arr = sorted([int(input()) for _ in range(N)])
-left, right = 0, 0
-min_val =  int(2*1e9)
 
-while left < N:
-    if right == N:
-        break
-    if arr[right]- arr[left] < M:
-        right += 1
-        continue
-    min_val = min(min_val,arr[right]-arr[left])
-    left += 1
-print(min_val)
+for i in range(N):
+    idx = bisect.bisect_left(arr,arr[i] + M)
+    if idx < N:
+        min_value = min(min_value,arr[idx]-arr[i])
+print(min_value)
