0b00f73e99fda75a12d3de7399ec4d6140783d67
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-02-07 14:45:04 +0900

    [Gold IV] Title: 점수따먹기, Time: 1472 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.py
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.py
@@ -1,32 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int,input().split())
+N, M = map(int, input().split())
 
-arr = [list(map(int,input().split())) for _ in range(N)]
-partial_arr = [[0 for _ in range(M)] for _ in range(N)]
-max_value = -10000
-for i in range(N):
-    for j in range(M):
-        partial_arr[i][j] = arr[i][j]
-        if i >= 1:
-            partial_arr[i][j] += partial_arr[i-1][j]
-        if j >= 1:
-            partial_arr[i][j] += partial_arr[i][j-1]
-        if i >= 1 and j >= 1:
-            partial_arr[i][j] -= partial_arr[i-1][j-1]
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-for sr in range(N):
-    for sc in range(M):
-        for fr in range(sr,N):
-            for fc in range(sc,M):
-                temp = partial_arr[fr][fc]
-                if sr >=1:
-                    temp -= partial_arr[sr-1][fc]
-                if sc >=1:
-                    temp -= partial_arr[fr][sc-1]
-                if sr>=1 and sc>=1:
-                    temp += partial_arr[sr-1][sc-1]
-                max_value = max(max_value,temp)
+max_value = -float('inf')  # 최댓값 초기화
+
+# top부터 bottom까지의 행들을 고정하여 처리
+for top in range(N):  
+    temp = [0] * M  # 각 열에 대한 부분 합을 저장할 배열
+    
+    # bottom 행을 고정하여, top 행부터 bottom 행까지의 합을 구함
+    for bottom in range(top, N):
+        # 각 열에 대해 해당하는 부분 합을 계산
+        for col in range(M):
+            temp[col] += arr[bottom][col]
         
+        # temp 배열에 대해 Kadane's Algorithm을 적용하여 최대 부분합을 구함
+        current_sum = 0
+        local_max = -float('inf')
+        
+        for value in temp:
+            current_sum += value
+            local_max = max(local_max, current_sum)
+            
+            if current_sum < 0:
+                current_sum = 0
+        
+        # 최댓값 갱신
+        max_value = max(max_value, local_max)
+
+# 출력
 print(max_value)
