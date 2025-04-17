45af91d22450c8e47e12129e2e102dbb0a047ada
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-04-17 10:34:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 19.25 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,21 +1,20 @@
-# 왼쪽상단끝 -> 오른쪽하단끝
-# 1 -> 지나갈 수 있음 / 0 -> 지나갈 수 없음
-# 가장 빠른 거리 return -> heapq 사용해서 도착 즉시 거리 return
-# n -> 행, m -> 열 다를 수도 있음
-# visited도 필요할 듯?
-import heapq
+from collections import deque
+
 def solution(maps):
-    N,M = len(maps), len(maps[0])
-    di = [[0,1],[1,0],[0,-1],[-1,0]]
-    queue = []
-    heapq.heappush(queue, [1,(0,0)])
+    N, M = len(maps), len(maps[0])
+    di = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    visited = [[False]*M for _ in range(N)]
+    queue = deque()
+    queue.append((0, 0, 1))  # (row, col, count)
+    visited[0][0] = True
+
     while queue:
-        cnt, (curr_r,curr_c) = heapq.heappop(queue)
-        if (curr_r,curr_c) == (N-1,M-1):
+        r, c, cnt = queue.popleft()
+        if (r, c) == (N-1, M-1):
             return cnt
-        for i in range(4):
-            nr, nc = curr_r + di[i][0], curr_c + di[i][1]
-            if 0<=nr<N and 0<=nc<M and maps[nr][nc] == 1:
-                maps[nr][nc] += 1
-                heapq.heappush(queue,[cnt+1,(nr,nc)])
+        for dr, dc in di:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < M and maps[nr][nc] == 1 and not visited[nr][nc]:
+                visited[nr][nc] = True
+                queue.append((nr, nc, cnt + 1))
     return -1
