f0629745607b3af83993b28de6765c112f31d0ac
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-05-06 16:03:11 +0900

    [level 3] Title: 야근 지수, Time: 452.41 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.38 MB, 시간: 905.51 ms
+메모리: 9.27 MB, 시간: 452.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 14:09:10
+2025년 05월 06일 16:03:08
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,16 +1,13 @@
+# 가장 작업량이 많은 일부터 해야함. -> 우선순위 큐 자료구조 사용
 import heapq
-
 def solution(n, works):
-    # 최대 힙을 만들기 위해 음수로 변환
     works = [-w for w in works]
     heapq.heapify(works)
-
-    while n > 0:
+    while n>0:
         now = heapq.heappop(works)
-        if now == 0:  # 실제 작업량 0이면 중단
+        if now == 0:
             break
-        heapq.heappush(works, now + 1)  # 작업량 감소 → 음수니까 +1
+        heapq.heappush(works,now+1)
         n -= 1
 
-    # 음수를 다시 양수로 바꾸고 제곱합 계산
-    return sum((w ** 2 for w in works))
+    return sum(map(lambda x: x**2, works))
