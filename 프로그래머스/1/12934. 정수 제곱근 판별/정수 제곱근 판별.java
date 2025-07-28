86ae34e49a4dabec9c60114c99be030291797478
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-07-29 01:03:28 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,13 +1,11 @@
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        double maxNum = Math.sqrt(n);
-        while(answer<=maxNum){
-            if(Math.pow(answer,2)==n){
-                return (long) Math.pow(answer+1,2);
-            }
-            answer+=1;
+        double sqrt = Math.sqrt(n);
+
+        if (sqrt == (long) sqrt) {
+            return (long) Math.pow(sqrt + 1, 2);
+        } else {
+            return -1;
         }
-        return -1;
     }
 }
