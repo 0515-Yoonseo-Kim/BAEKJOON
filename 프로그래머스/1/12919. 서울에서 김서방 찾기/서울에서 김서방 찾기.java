bc0aa2186b1b00149824dec025fc6b0f85357653
0515-Yoonseo-Kim <156681108+0515-Yoonseo-Kim@users.noreply.github.com>
2025-07-29 17:15:54 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.20 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,8 @@
+import java.util.*;
 class Solution {
     public String solution(String[] seoul) {
         // seoul 배열에서 문자열 "Kim"을 찾으면 정답 String에 대입해서 반환.
-        int idx = 0;
-        for(int i = 0; i<seoul.length; i++){
-            if(seoul[i].equals("Kim")){
-                idx=i;
-                break;
-            }
-            
-        }
+        int idx = Arrays.asList(seoul).indexOf("Kim");
         
         String answer = String.format("김서방은 %d에 있다",idx);
         return answer;
